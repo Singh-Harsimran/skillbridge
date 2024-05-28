ed9d796dace9a3a5817d8678f0f488ce9bea3efb
Harsimran Singh <simran381@gmail.com>
2024-05-28 15:07:26 -0700

    header height

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
 export default function Header(  ) {
     return (<>
     <div className='skillbridge-header md:tw-flex '>
-        <div className="tw-flex tw-items-center ms:tw-justify-center tw-text-center tw-flex-none md:tw-w-[300px] tw-text-2xl tw-font-bold tw-bg-lightBG tw-p-3">
+        <div className="tw-flex tw-items-center ms:tw-justify-center tw-text-center tw-flex-none md:tw-w-[300px] tw-text-2xl tw-font-bold tw-bg-lightBG tw-p-4">
             SKILLBRIDGE
         </div>
-        <div  className="tw-p-3 tw-bg-darkBG tw-grow tw-relative md:tw-ps-20 tw-flex tw-items-center tw-justify-center" >
+        <div  className="tw-p-4 tw-bg-darkBG tw-grow tw-relative md:tw-ps-20 tw-flex tw-items-center tw-justify-center" >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 52 128" 
             className="tw-hidden md:tw-block tw-text-lightBG tw-absolute tw-left-0 tw-top-0 tw-bottom-0 tw-h-full">
                 <path d="m51.5 0h-51.5v127.5z" fill="currentColor"></path></svg>
